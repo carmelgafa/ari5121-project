70fdecf0bda2d5dd0ef43e98bcaa0c275ba55891
Carmel Gafa <carmelgafa@gmail.com>
2025-04-11 08:42:53 +0200

    vs - embeddings extraction fix - working to test script

--- VoiceSimilarityAnalysis-code/calculate_population_embeddings.py
+++ VoiceSimilarityAnalysis-code/calculate_population_embeddings.py
@@ -9,23 +9,35 @@ str: the name of the speaker
 list: a list of lists, where each list contains the embeddings
 for a single file.
 The embeddings are stored in a list of floats.
+
+Finally all the data is stored in a pandas dataframe and saved to a csv file.
 '''
 
 import os
 import re
-import shutil
+import torch
+import torchaudio
+import pandas as pd
+from transformers import Wav2Vec2FeatureExtractor, WavLMForXVector
 
 def create_embeddings_information():
     '''
+    Goes through all the speakers and calculate
+    the embeddings for all their files, in order to facilitate the 
+    comparison of the voices.
     '''
 
-    raw_accents_folder = os.path.join(
+    embeddings_dict = {}
+
+    local_model_folder = os.path.join(
         os.path.dirname(__file__),
-        'data',
-        'raw',
-        'ABI-1_Corpus',
-        'ABI-1 Corpus',
-        'accents')
+        'model',
+        'models--microsoft--wavlm-base-plus-sv',
+        'snapshots',
+        'feb593a6c23c1cc3d9510425c29b0a14d2b07b1e')
+
+    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(local_model_folder)
+    model = WavLMForXVector.from_pretrained(local_model_folder)
 
     cleansed_folder = os.path.join(
         os.path.dirname(__file__),
@@ -34,46 +46,107 @@ def create_embeddings_information():
 
     # list accent folders removing the annoying folders
     accents = [accent_folder
-               for accent_folder in os.listdir(raw_accents_folder)
-               if not accent_folder.startswith('.')]
+               for accent_folder in os.listdir(cleansed_folder)]
 
     # go through all accents
     for accent in accents:
-        accent_path = os.path.join(raw_accents_folder, accent)
+        accent_path = os.path.join(cleansed_folder, accent)
 
         # list all genders in each accent
         genders = [gender_folder
-                   for gender_folder in os.listdir(accent_path)
-                   if not gender_folder.startswith('.')]
+                   for gender_folder in os.listdir(accent_path)]
 
         # go through all genders in each accent
-        for gender in genders:  
+        for gender in genders:
             gender_folder = os.path.join(accent_path, gender)
             # go through each speaker in each gender
             # list all speakers
 
             speakers = [speaker
-                        for speaker in os.listdir(gender_folder)
-                        if not speaker.startswith('.')]
+                        for speaker in os.listdir(gender_folder)]
 
             for speaker in speakers:
                 speaker_path = os.path.join(gender_folder, speaker)
 
-                # store resulting  data in cleaned/accent/gender/speaker
-                dest_path = os.path.join(cleansed_folder, accent, gender, speaker)
-                os.makedirs(dest_path, exist_ok=True)
-
-                # copy only filenames that  are shortpassage*.wav
-                # go through all files
                 for filename in os.listdir(speaker_path):
                     if re.fullmatch(r'shortpassage.*\.wav', filename):
 
-                        src_file = os.path.join(speaker_path, filename)
-                        dst_file = os.path.join(dest_path, filename)
-                        shutil.copy2(src_file, dst_file)
+                        print(accent, gender, speaker, filename)
+
+                        waveform, original_sr = torchaudio.load(
+                            os.path.join(
+                                speaker_path,
+                                filename))
+
+                        # Resample to 16kHz if needed
+                        target_sr = 16000
+                        if original_sr != target_sr:
+                            resampler = torchaudio.transforms.Resample(
+                                orig_freq=original_sr,
+                                new_freq=target_sr)
+                            waveform = resampler(waveform)
+
+                        # Convert to 1D numpy array
+                        audio_array = waveform.squeeze().numpy()
+
+                        # Extract features
+                        inputs = feature_extractor(
+                            [audio_array],
+                            sampling_rate=target_sr,
+                            return_tensors='pt',
+                            padding=True)
+
+                        with torch.no_grad():
+                            embeddings = model(**inputs).embeddings
+                            embeddings = torch.nn.functional.normalize(embeddings, dim=-1)
+
+                        if speaker not in embeddings_dict:
+                            embeddings_dict[speaker] = []
+
+                        embeddings_dict[speaker].append(embeddings.squeeze().numpy())
+
+
+    # Convert to a pandas df
+    df = pd.DataFrame.from_dict(embeddings_dict, orient='index')
+
+    embedding_folder = os.path.join(
+        os.path.dirname(__file__),
+        'data',
+        'embeddings')
+
+    if not os.path.exists(embedding_folder):
+        os.makedirs(embedding_folder, exist_ok=True)
+
+    # Save to a CSV file
+    df.to_csv(os.path.join(embedding_folder, 'embeddings.csv'), index=True)
+
+
+
+def check_embeddings_count():
+
+    # read embeddings omitting index and headers
+
+    df = pd.read_csv(os.path.join(
+        os.path.dirname(__file__),
+        'data',
+        'embeddings',
+        'embeddings.csv'),
+        index_col=0,
+        header=0)
+
+    rec_1 = df.head(1)
+
+    #print shape of element in second column of first row
+    print('Number of embeddings:', rec_1.iloc[0][2].shape)
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    print('Cleansing ABI-1 Corpus dataset...')
-    create_embeddings_information()
-    print('Cleansing completed.')
+    # print('Calculating embeddings for all speakers...')
+    # create_embeddings_information()
+
+    check_embeddings_count()
--- VoiceSimilarityAnalysis-code/compare_voices.py
+++ VoiceSimilarityAnalysis-code/compare_voices.py
@@ -1,6 +1,9 @@
 '''
-This script compares two WAV files using WavLM and returns
-the cosine similarity between their embeddings.
+This script contains scripts with smaller cases of voice similarity analysis
+and embedding extraction
+
+The purpose is to ensure that the algorithms are working correctly and to test the
+functionality of the code.
 '''
 
 import os
@@ -8,6 +11,7 @@ import torch
 import torchaudio
 from transformers import Wav2Vec2FeatureExtractor
 from transformers import WavLMForXVector
+import numpy as np
 
 
 def get_embeddings_for_wav(feature_extractor, model, accent, gender, speaker, wav_file):
@@ -89,6 +93,66 @@ def cosine_sim_two_wav(wav_1, wav_2):
 
     return similarity
 
+def create_embeddings_dict(wav_1, wav_2):
+    '''
+    Compare two WAV files using WavLM and return the cosine similarity between their embeddings.
+    '''
+
+    dict_embeddings = {}
+
+    local_model_folder = os.path.join(
+        os.path.dirname(__file__),
+        'model',
+        'models--microsoft--wavlm-base-plus-sv',
+        'snapshots',
+        'feb593a6c23c1cc3d9510425c29b0a14d2b07b1e')
+
+    feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(local_model_folder)
+    model = WavLMForXVector.from_pretrained(local_model_folder)
+
+    e_1 = get_embeddings_for_wav(
+        feature_extractor,
+        model,
+        wav_1['accent'],
+        wav_1['gender'],
+        wav_1['speaker'],
+        wav_1['wav_file'])
+
+    speaker = wav_1['speaker']
+    if speaker not in dict_embeddings:
+        dict_embeddings[speaker] = np.empty((0,512))
+
+    e1_np = e_1.squeeze().numpy()
+
+    print(f"Speaker: {speaker}, Embedding Shape: {e1_np.shape}")
+
+    dict_embeddings[speaker] = np.append(dict_embeddings[speaker], [e_1.squeeze().numpy()], axis=0)
+
+    e_2 = get_embeddings_for_wav(
+        feature_extractor,
+        model,
+        wav_2['accent'],
+        wav_2['gender'],
+        wav_2['speaker'],
+        wav_2['wav_file'])
+
+    print(f"Speaker: {speaker}, Embedding Shape: {e1_np.shape}")
+
+
+    speaker = wav_2['speaker']
+    if speaker not in dict_embeddings:
+        dict_embeddings[speaker] = np.empty((0,512))
+
+    dict_embeddings[speaker] = np.append(dict_embeddings[speaker], [e_2.squeeze().numpy()], axis=0)
+
+    for speaker, embeddings in dict_embeddings.items():
+        #get the dimension of the embeddings
+        embedding_dim = embeddings.shape
+        print(f"Speaker: {speaker}, Embedding Dimension: {embedding_dim}")
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -103,7 +167,7 @@ if __name__ == '__main__':
         'accent': 'brm_001',
         'gender': 'male',
         'speaker': 'ajh001',
-        'wav_file': 'shortpassage_CT.wav'
+        'wav_file': 'shortpassageb_CT.wav'
     }
 
     sim = cosine_sim_two_wav(wav_1_data, wav_2_data)
@@ -111,3 +175,5 @@ if __name__ == '__main__':
     print(f'''Cosine similarity between {wav_1_data['speaker']} 
           using {wav_1_data['wav_file']} and {wav_2_data['speaker']} 
           using {wav_2_data['wav_file']}: {sim.item()}''')
+
+    create_embeddings_dict(wav_1_data, wav_2_data)
